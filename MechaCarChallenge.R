f82838faddca27cf2a3bfeda5411df0b93659536
Lane Vermunt <111530580+ldvermunt@users.noreply.github.com>
2022-12-08 09:26:03 -0500

    Commit after deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,10 @@
+# Deliverable 1
 library(dplyr) #Import library
 MPG_table <- read.csv(file='MechaCar_mpg.csv',check.names = F, stringsAsFactors = F) # read in MPG .csv
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = MPG_table) #Perform linear Regression
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = MPG_table)) #Get Summary Statistics.
+
+# Deliverable 2
+coil_table <- read.csv(file='Suspension_Coil.csv',check.names = F, stringsAsFactors = F) # read in Susp_Coil.csv
+total_summary <- coil_table %>% summarise(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #Make table for psi summary data
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarise (Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #Make lot_summary table
